2a0e0209b1c70d5cfe04af46b7c794d4d16a42c4
Wonhee Shin <wonhshin@c4r10s6.42seoul.kr>
2023-11-15 18:43:00 +0900

    pb

--- ex01/phone.cpp
+++ ex01/phone.cpp
@@ -27,68 +27,100 @@ public:
 	std::string getDs() const { return ds; }
 };
 
-
 void	Contact::infoAdd()
 {
-	std::cout << "Phone NUMBER: ";
+	std::cout << "[Phone NUMBER]:   ";
 	std::getline(std::cin, pnum);
-	std::cout << "First NAME: ";
+	if (pnum.empty())
+	{
+		while (1)
+		{
+			if (!pnum.empty())
+				break ;
+			std::cout << "YOU HAVE TO FILL IT// IF YOU WANT TO GO NEXT STEP\n";
+			std::getline(std::cin, pnum);
+		}
+	}
+	if (std::cin.eof())
+		exit(1);
+	std::cout << "[First NAME]  :   ";
 	std::getline(std::cin, fname);
-	std::cout << "Last NAME: ";
+	if (std::cin.eof())
+		exit(1);
+	std::cout << "[Last NAME]   :   ";
 	std::getline(std::cin, lname);
-	std::cout << "Nick NAME: ";
+	if (std::cin.eof())
+		exit(1);
+	std::cout << "[Nick NAME]   :   ";
 	std::getline(std::cin, aka);
-	std::cout << "Dark Secret {Dont't worry. Its first Secret}: ";
+	if (std::cin.eof())
+		exit(1);
+	std::cout << "[Dark Secret {Dont't worry. Its first Secret}]: ";
 	std::getline(std::cin, ds);
+	if (std::cin.eof())
+		exit(1);
 }
+
 class	PhoneBook
 {
 private:
 	Contact ct[INFO_CONST::MAX_LEN];
 public:
-	int idx = 0;
+	int idx;
+	int total_idx;
+	PhoneBook()
+	{
+		idx = 0;
+		total_idx = 0;
+	};
 	std::string input;
-	void addContact() {
-        if (idx < INFO_CONST::MAX_LEN) {
-            ct[idx % 8].infoAdd();
-        }
-        idx++;
-    }
+	void	addContact();
 	void	printPB(int idx);
 	void	rulePB(std::string text);
 	void	searchPB();
 };
 
+void	PhoneBook::addContact()
+{
+	total_idx++;
+	if (total_idx >= INFO_CONST::MAX_LEN)
+		total_idx = INFO_CONST::MAX_LEN;
+	if (idx >= INFO_CONST::MAX_LEN)
+	 	idx = idx % INFO_CONST::MAX_LEN ; 
+	ct[idx].infoAdd();	
+	idx++;
+}
+
 void	PhoneBook::printPB(int i)
 {
-	std::cout << "Phone num : " << ct[i].getPnum() << std::endl;
-	std::cout << "Last Name : " << ct[i].getLname() << std::endl;
-	std::cout << "Aka : " << ct[i].getAka() << std::endl;
-	std::cout << "First Name : " << ct[i].getFname() << std::endl;
-	std::cout << "Darkest Secret : " << ct[i].getDs() << std::endl;
+	std::cout << "[Phone num]      : " << ct[i].getPnum() << std::endl;
+	std::cout << "[Last Name]      : " << ct[i].getLname() << std::endl;
+	std::cout << "[A.k.a]          : " << ct[i].getAka() << std::endl;
+	std::cout << "[First Name]     : " << ct[i].getFname() << std::endl;
+	std::cout << "[Darkest Secret] : " << ct[i].getDs() << std::endl;
 }
 
 void	PhoneBook::rulePB(std::string text)
 {
 	if (text.length() <= 10)
-		std::cout << std::right << text;
+		std::cout << std::setw(10) << text;
 	else
-		std::cout << std::setw(10) << text.substr(0,8) + ".";
+		std::cout << std::setw(10) << text.substr(0, 9) + ".";
 }
 
 void	PhoneBook::searchPB()
 {
 	if (this->idx == 0)
-		std::cout << "PhoneBook is EMPTY!!" << std::endl;
+		std::cout << "❗️PhoneBook IS EMPTY💌 YOU CAN FILL IT UP\n" << std::endl;
 	else 
 	{
 		int i = -1;
 		int selectIdx;
 
-		while (++i < idx)
+		while (++i < total_idx)
 		{
 			std::cout << "|";
-			std::cout << i + 1 << "|";
+			std::cout << std::setw(10) << i + 1 << "|";
 			rulePB(ct[i].getFname());
 			std::cout << "|";
 			rulePB(ct[i].getLname());
@@ -98,29 +130,37 @@ void	PhoneBook::searchPB()
 		}
 		std::cout << "Select Index: "<< std::endl;
 		std::cin >> selectIdx;
+		if (std::cin.eof())
+			exit(1);
+		std::cin.ignore();
+		std::cin.clear();
 		printPB(selectIdx - 1);
 	}
 }
 
-
-
 int main(void)
 {
 	PhoneBook pb;
-
+	
+	std::cout << "\t💘 ☪️[MISSION] COMPLETE MY PHONE BOOK📱🍑\n" << std::endl;
 	while (1)
 	{
-		std::cout << "Command : ADD, SEARCH, EXIT" << std::endl;
+		std::cout << "COMMAND [ADD] [SEARCH] [EXIT] \n YOU MUST CHOOSE THIS ORDER __ IF NOT,, YOU DESERVE IT🐣" << std::endl;
+		if (std::cin.eof())
+			exit(1);
 		std::cin>>pb.input;
 		std::cin.ignore();
-		if (pb.input == "ADD")
+		if (pb.input == "ADD" || pb.input == "add")
 			pb.addContact();
-		else if (pb.input == "EXIT")
-			break ;
-		else if (pb.input == "SEARCH")
+		else if (pb.input == "SEARCH" || pb.input == "search")
 			pb.searchPB();
+		else if (pb.input == "EXIT" || pb.input == "exit")
+			break ;
 		else
-			std::cout<< "NOT VERIFY COMMAND" << std::endl;
+		{
+			std::cout<< "------😈-------NOT VERIFY COMMAND------😈--------" << std::endl;
+			std::cout<< "😈😈😈😈😈😈😈😈😈😈😈😈😈😈😈😈😈👿😈😈😈😈😈😈😈😈😈😈😈\n\n" << std::endl;
+		}
 	}
 	return 0;
 }
