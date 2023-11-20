3a4e6ff523ccf21ccc7f2bffdcadf92ed675e5f8
Wonhee Shin <wonhshin@c4r10s2.42seoul.kr>
2023-11-20 19:44:15 +0900

    checkdynamic

--- cpp_module01/ex00/srcs/main.cpp
+++ cpp_module01/ex00/srcs/main.cpp
@@ -1,16 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wonhshin <wonhshin@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/20 18:21:18 by wonhshin          #+#    #+#             */
+/*   Updated: 2023/11/20 19:41:41 by wonhshin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../inc/Zombie.hpp"
+
+
+void    displayZombie(Zombie *zb)
+{
+    if (zb) {
+        std::cout << "display zb : " << zb->getName() << std::endl;
+    }
+}
+
+void    displayZombies(Zombie &zb)
+{
+        std::cout << "display zbs : " << zb.getName() << std::endl;
+}
 
 int main(void)
 {
-    Zombie zb("FOO");
-    
-    zb.announce();
-
+    Zombie zb("Foo");
     Zombie *newZomb = newZombie("Wonei");
+
+    displayZombie(newZomb);
+    displayZombies(zb);
+    zb.announce();
     newZomb->announce();
+    randomChump("Konie");
+
     delete  newZomb;
-
-    randomChump("Kcian");
-
     return (0);
 }
