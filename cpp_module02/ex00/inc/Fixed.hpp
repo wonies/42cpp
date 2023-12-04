f30448befef5ec1df18593930007653495fbca95
Wonhee Shin <wonhshin@c4r10s1.42seoul.kr>
2023-12-04 19:27:54 +0900

    cpy

--- cpp_module02/ex00/inc/Fixed.hpp
+++ cpp_module02/ex00/inc/Fixed.hpp
@@ -7,42 +7,15 @@
 class Fixed
 {
 private:
-	int fixedInt;
+	int value;
 	static const int bits = 8;
 public:
-	Fixed()
-	{
-		this->fixedInt = 0;
-		std::cout << "Default constructor called" << std::endl;
-	};
-	Fixed(const Fixed& obj)
-	{
-		std::cout << "Copy constructor called" << std::endl;
-		this->fixedInt = obj.getRawBits();
-	};
-	Fixed &operator= (const Fixed &obj)
-	{
-		std::cout << "Copy assignment operator called" << std::endl;
-		if (this != &obj)
-		{
-			this->fixedInt = obj.getRawBits();
-		}
-		return *this;
-	};
-	~Fixed()
-	{
-		std::cout << "Destructor called" << std::endl;
-	};
-	int getRawBits( void ) const
-	{
-		std::cout << "getRawBits member function called" << std::endl;
-		return this->fixedInt;
-	};
-	void setRawBits( int const raw )
-	{
-		this->fixedInt = raw;
-		std::cout << this->fixedInt << std::endl;
-	};
+	Fixed();
+	Fixed(const Fixed& obj);
+	Fixed &operator= (const Fixed &obj);
+	~Fixed();
+	int getRawBits( void ) const;
+	void setRawBits( int const raw );
 
 };
 
--- cpp_module02/ex00/srcs/main.cpp
+++ cpp_module02/ex00/srcs/main.cpp
@@ -12,6 +12,5 @@ int main( void )
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
 
-
 	return 0;
 }
