947887c2a5d132a095aff248c9aa9941b66007da
Wonhee Shin <wonhshin@c4r10s7.42seoul.kr>
2023-12-23 18:47:08 +0900

    diamond

---
+++ cpp_module03/ex03/srcs/DiamondTrap.cpp
@@ -0,0 +1,28 @@
+#include "DiamondTrap.hpp"
+
+DiamondTrap::DiamondTrap(void) {}
+
+DiamondTrap::DiamondTrap(std::string _name)
+    : ClapTrap(_name + "_clap_name"),
+      FragTrap(_name),
+      ScavTrap(_name),
+      Name(_name) {
+  std::cout << "💎Diamond_Trap_CONSTRUCTOR💎" << std::endl;
+}
+DiamondTrap::DiamondTrap(DiamondTrap const &instance) { *this = instance; }
+
+DiamondTrap &DiamondTrap::operator=(const DiamondTrap &rhs) {
+  if (this != &rhs) {
+    ClapTrap::operator=(rhs);
+  }
+  return *this;
+}
+
+DiamondTrap::~DiamondTrap(void) {
+  std::cout << "💎Diamond_TRAP__DESTRUCTOR💎" << std::endl;
+}
+
+void DiamondTrap::whoAmI(void) {
+  std::cout << "Base' Name \t" << getName() << std::endl;
+  std::cout << "Diamond's Name \t" << Name << std::endl;
+}
