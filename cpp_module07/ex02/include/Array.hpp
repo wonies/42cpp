03eb75c532559eb54f77399da13abf91f7d8d3b2
Wonhee Shin <wonhshin@c4r10s7.42seoul.kr>
2024-01-23 20:12:34 +0900

    cpp07

---
+++ cpp_module07/ex02/include/Array.hpp
@@ -0,0 +1,19 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+template <typename T>
+
+class Array {
+ public:
+  Array(void);
+  Array(**replace parameters **);
+  Array(Array const &instance);
+  Array &operator=(Array const &rhs);
+  ~Array(void);
+
+ private:
+};
+
+// Array<T>::
+
+#endif
