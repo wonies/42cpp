ca4e94d36bb07036f42ae0967818bb1c09cc4048
Wonhee Shin <wonhshin@c4r10s7.42seoul.kr>
2024-01-24 14:46:08 +0900

    array

--- cpp_module07/ex02/include/Array.hpp
+++ cpp_module07/ex02/include/Array.hpp
@@ -1,19 +1,34 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
-template <typename T>
+#include <iostream>
 
+template <typename T>
 class Array {
  public:
-  Array(void);
-  Array(**replace parameters **);
-  Array(Array const &instance);
-  Array &operator=(Array const &rhs);
-  ~Array(void);
+  Array(void) : _arr(new T[0]), _n(0) {}
+  Array(unsigned int n) : _n(n) { _arr = new T[n]; }
+  Array(Array const &instance) : _arr(new T[instance._n]), _n(instance._n) {
+    *this = instance;
+  }
+  Array &operator=(Array const &rhs) {
+    if (this != &rhs) {
+      delete[] _arr;
+      _n = rhs._n;
+      _arr = new T[rhs._n];
+      for (unsigned int i = 0; i < _n; i++) _arr[i] = rhs._arr[i];
+    }
+    return *this;
+  }
+  ~Array(void) { delete[] _arr; }
+  T &operator[](unsigned int val) {
+    if (val >= _n || val < 0) throw std::out_of_range("Index : out of range");
+    return _arr[val];
+  }
 
  private:
+  T *_arr;
+  unsigned int _n;
 };
-
-// Array<T>::
 
 #endif
